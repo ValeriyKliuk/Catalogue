2c0d6a411a173b383e9f0a23b23f3b88e2761321
Valeriy Kliuk <valeriy.kliuk@r3d.com>
2017-05-02 08:09:05 -0400

    modified ProductColor.swift

---
+++ Catalogue/Catalogue/Interactor/ProductColor.swift
@@ -0,0 +1,21 @@
+//
+//  ProductColor.swift
+//  Catalogue
+//
+//  Created by Valeriy Kliuk on 2017-04-29.
+//  Copyright © 2017 Valeriy Kliuk. All rights reserved.
+//
+
+import Foundation
+
+/// Product Color
+enum ProductColor {
+    /// Blue
+    case blue
+    /// Brown
+    case brown
+    /// Green
+    case green
+    /// Orange
+    case orange
+}
